c13bf10b7c117d0502c59ac1af45c4fc07de9cb2
cboissel <56295894+cboissel@users.noreply.github.com>
2020-08-20 16:27:34 +0200

    Cleaner version 2

--- src/test_Camille.py
+++ src/test_Camille.py
@@ -19,10 +19,7 @@ import household
 
 # %% CREATION ECONOMIE
 
-n=1
-
 # Variables statiques Firms
-
 firms_args={
         "z":np.random.uniform(5,5,1),
         "sigma":np.random.uniform(0,1,1),
@@ -30,7 +27,7 @@ firms_args={
         "alpha_p":0.08,
         "beta":0.2,
         "beta_p":0.05,
-        "w":0.02
+        "w":0.2
         }
 
 # Variables statiques Household
@@ -49,10 +46,10 @@ econ_args = {
         'd':1,
         'netstring':'regular',
         'directed':True,
-        'j0':np.array([3]),
-        'j1':np.array([5]),
+        'j0':np.array([5]),
+        'j1':np.array([2]),
         'a0':np.ones(1)*0.5,
-        'q':0.5,
+        'q':0,
         'b':0.95
         }
 
@@ -61,16 +58,21 @@ econ_args = {
 p_init=5
 g_init=np.array([3])
 
+#p_eq_dis_0
+#g_eq_dis_0
+
 dyn_args={
-        'p0':np.array([p_init]),#np.random.uniform(1,2,econ_args['n']),
+        'p0':np.array([p_eq_dis_0]),#np.random.uniform(1,2,econ_args['n']),
         'w0':1,
-        'g0':g_init,
-        's0':np.random.uniform(0,0,econ_args['n']),
-        't1':g_init,
+        'g0':g_eq_dis_0,
+        's0':np.random.uniform(0,0,1),
+        't1':g_eq_dis_0,
         'B0':random.randint(0,0)
         }
 
-
+# Variables de suivi des perturbations pour un dossier donné (last: 20_08_2020)
+state="nd"
+compteur=4
 
 # %% %% SIMULATION
 # Création objet classe Economy
@@ -102,11 +104,13 @@ sim.eco.compute_eq()
 print("P_EQ", sim.eco.p_eq)
 print("G_EQ",sim.eco.g_eq)
 
-p_eq_dis_0=sim.eco.p_eq[1] + random.uniform(-10**(-5),10**(-5))
-g_eq_0=sim.eco.g_eq[0]
+p_eq_0=sim.eco.p_eq
+g_eq_0=sim.eco.g_eq
+p_eq_dis_0=sim.eco.p_eq + random.uniform(-10**(-5),10**(-5))
+g_eq_dis_0=sim.eco.g_eq + random.uniform(-10**(-5),10**(-5))
 
-p_eq_dis_1=sim.eco.p_eq[1] + random.uniform(-10**(-5),10**(-5))
-g_eq_1=sim.eco.g_eq[1]
+#p_eq_dis_1=sim.eco.p_eq[1] + random.uniform(-10**(-5),10**(-5))
+#g_eq_1=sim.eco.g_eq[1]
 
 if sim.eco.b!=1 and sim.eco.q>0:
     print("ATTENTION A LA PRECISION DE L'EQUILIBRE")
@@ -120,22 +124,25 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 ### Prices
-plt.title("Prices of the firm's production")
-plt.xlabel('Time')
-plt.ylabel('P1')
+fig, ax = plt.subplots()
+ax.set_title("Prices of the firm's production")
+ax.set_xlabel('Time')
+ax.set_ylabel('P1')
 
-plt.plot(sim.prices[1:-1])
+ax.plot(sim.prices[1:-1])
 
-plt.axhline(y=p_eq[0],linewidth=1.3, alpha=1, color="green", label="p=p_eq")
-plt.axhline(y=p_eq[1],linewidth=1.3, alpha=1, color="red", label="p=p_eq")
+plt.axhline(y=p_eq_0,linewidth=1.3, alpha=1, color="green", label="p=p_eq")
+#plt.axhline(y=sim.eco.p_eq[1],linewidth=1.3, alpha=1, color="red", label="p=p_eq")
 #plt.xscale("linear")
-plt.yscale("log")
+ax.set_yscale("log")
 
 #plt.grid(True)
 plt.show()
 
 #MKDIR NOT DONE: plt.savefig("/mnt/research-live/user/cboissel/network-economy/OneFirmCase_Images_v1/prices.png")
+directoire="/Users/boisselcamille/Documents/Stage_Econophysix/networks_code/OneFirmCase_Images_v1/2020_08_20_PerturbationsEquilibres_b<1_q=0/"+str(compteur)+"prices_b="+str(econ_args["b"])+"_q="+str(econ_args["q"])+"_"+state+".png"                
 
+fig.savefig(directoire)
 
 #%% PLOT EQUILIBRIUM ON PRODUCTION 
 
@@ -152,11 +159,13 @@ prod=[sim.Q_demand[i,1,1]+sim.Q_demand[i,1,0] for i in range(len(sim.Q_demand))]
 
 ax.plot(prod[1:-1])
 
-plt.axhline(y=sim.eco.g_eq[0]*sim.eco.firms.z,linewidth=1.3, alpha=1, color="green", label="prod=prod_eq")
+plt.axhline(y=g_eq_0*sim.eco.firms.z,linewidth=1.3, alpha=1, color="green", label="prod=prod_eq")
 
 #plt.xscale("linear")
 ax.set_yscale("log")
-ax.set_ylim(0,float(max(sim.labour))+100)
+#ax.set_ylim(0,float(max(prod))+100)
 #plt.grid(True)
 plt.show()
 
+directoire="/Users/boisselcamille/Documents/Stage_Econophysix/networks_code/OneFirmCase_Images_v1/2020_08_20_PerturbationsEquilibres_b<1_q=0/"+str(compteur)+"prods_b="+str(econ_args["b"])+"_q="+str(econ_args["q"])+"_"+state+".png"                
+fig.savefig(directoire)
