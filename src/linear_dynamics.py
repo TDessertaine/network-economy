47261de48d3ebc63db93dcd260fa893e54d342c2
TDessertaine <theo.dessertaine@polytechnique.edu>
2021-02-26 18:21:15 +0100

    Method to compute eigenvalues in large prodcutivity limit

--- src/linear_dynamics.py
+++ src/linear_dynamics.py
@@ -11,7 +11,7 @@ def canonical_Rn(n, i):
 def canonical_Mn(n, i, j):
     a = np.zeros((n, n))
     a[i, j] = 1
-    return spr.bsr_matrix(a)
+    return spr.bsr_matrix(np.outer(canonical_Rn(n, i)), np.outer(canonical_Rn(n, j)))
 
 
 def constant_line_one(n, i):
@@ -21,45 +21,56 @@ def constant_line_one(n, i):
 class LinearDynamics:
 
     @staticmethod
-    def p_pol_mul(alpha, alphap, beta, betap, omega, b, f, r):
-        return np.array([1,
-                         -(2 - alpha - betap - beta + alphap * beta / b),
-                         (1 - alpha) * (1 - beta - betap) + alpha * beta + (beta + betap) * alphap / b,
-                         -alpha * (beta + betap)])
+    def infinite_eps_pol1(alpha, alphap, beta, betap, sigma, b):
+        e = np.exp(-sigma)
+        delta_14 = betap * e + alpha * beta
+        return np.poly1d(np.polyadd(np.polymul(np.array([1, -e]),
+                                     np.array([1,
+                                               -(2 - alpha - betap - beta + alphap * beta / b),
+                                               (1 - alpha) * (1 - beta - betap) + alpha * beta + (
+                                                       beta + betap) * alphap / b + e * betap * (1 + alphap / b),
+                                               -alpha * (beta + betap) - betap * e])
+                                     ),
+                          e * np.array([
+                              alpha,
+                              delta_14 * (1 + alphap / b) - alpha * (1 - beta - betap + alphap * beta / b),
+                              -(delta_14 + alpha * betap)])))
 
     @staticmethod
-    def p_pol(alpha, alphap, beta, betap, omega, b, f, r):
-        return np.array([1,
-                         -(2 - alpha - betap - beta + alphap * beta / b),
-                         (1 - alpha) * (1 - beta - betap) + alpha * beta + (beta + betap) * alphap / b,
-                         -alpha * (beta + betap), 0]) - \
-               (1 - f) * (1 + r) * np.array([0,
-                                             1,
-                                             -(2 - alpha - betap - beta + alphap * beta / b),
-                                             (1 - alpha) * (1 - beta - betap) + alpha * beta + (
-                                                     beta + betap) * alphap / b,
-                                             -alpha * (beta + betap)])
+    def infinite_eps_pol2(alpha, alphap, beta, betap, omega, sigma, b, f, r, phi):
+        L = (1 + r) * (1 - f)
+        tau = phi * L / (phi + 1 - L)
+        e = np.exp(-sigma)
+        delta_11 = beta * (omega + alphap * (1 - L)) - betap * e * (1 - L)
+        delta_13 = beta * (1 - alpha) - betap * e - beta - betap
+        delta_14 = betap * e + alpha * beta
+        delta_hat = tau * (beta * omega + (alpha * beta + beta + betap) * (1 - L))
+        delta_tilde = tau * (omega + alpha * (1 - L))
+        pol1 = np.array([-delta_11, (1 - alpha) * delta_11 - delta_13 * (omega + alphap * (1 - L)) + delta_hat,
+                         -(1 - alpha) * delta_hat + delta_13 * delta_tilde])
+        pol2 = np.array([(1 - L) * delta_14,
+                         delta_14 * (omega - (1 - alpha - alphap) * (1 - L)) + alpha * ((1 - L) * delta_13 - delta_11),
+                         alpha * delta_hat - delta_14 * delta_tilde])
+        pol = np.polyadd(np.polymul([1, -e], pol1), +e * pol2)
+        return np.poly1d(np.polymul([-1, 0], pol) / b)
 
-    @staticmethod
-    def q_pol(alpha, alphap, beta, betap, omega, b, f, r, phi):
-        alphap_f = alphap * (1 - (1 + r) * (1 - f))
-        tau = phi * (1 + r) * (1 - f) / (phi + 1 - (1 + r) * (1 - f))
-        tau_over_one_minus_f = phi * (1 + r) / (phi + 1 - (1 + r) * (1 - f))
-        rho = -omega * beta * (1 + r - tau_over_one_minus_f) + beta * (1 - (1 + r) * (1 - f)) * (
-                alpha * tau_over_one_minus_f - alphap * (1 + r))
-        prefac = tau * (1 - (1 + r) * (1 - f)) * (beta + betap) + (1 - f) * rho
-        return f * (np.array([0,
-                              beta * (omega + alphap_f),
-                              -(beta + betap) * (alphap_f + omega),
-                              0, 0]) - (1 + r) * (1 - f) * np.array([0, 0,
-                                                                     beta * (omega + alphap_f),
-                                                                     -(beta + betap) * (alphap_f + omega),
-                                                                     0]) +
-                    np.array([0, 0,
-                              prefac,
-                              (1 - alpha) * prefac,
-                              0])
-                    ) / (b * (1 - (1 + r) * (1 - f)))
+    @classmethod
+    def theoretical_eigenvalues_infinite_eps(cls, alphav, alphapv, betav, betapv, omega, sigmav, kappa, b, f, r, phi, lseq):
+        if not(len(alphav) == len(alphapv) == len(betav) == len(betapv) == len(sigmav) == len(kappa)):
+            raise ValueError('All input arrays must be of same length.')
+
+        p = np.poly1d([1])
+        for k in range(len(alphav)):
+            p = p * cls.infinite_eps_pol1(alphav[k], alphapv[k], betav[k], betapv[k], sigmav[k], b)
+
+        s = np.poly1d([0])
+        for k in range(len(kappa)):
+            s = s + kappa[k] * cls.infinite_eps_pol2(alphav[k], alphapv[k], betav[k], betapv[k], omega, sigmav[k],
+                                                     b, f, r, phi) \
+                * (p / cls.infinite_eps_pol1(alphav[k], alphapv[k], betav[k], betapv[k], sigmav[k], b))[0]
+        s = s / (b * lseq)
+        final_polynomial = np.poly1d([1, -(1 + r)*(1 - f)]) * p + s
+        return final_polynomial.roots()
 
     def __init__(self, e):
         # Underlying economy
@@ -92,10 +103,10 @@ class LinearDynamics:
                                         np.diag(1. / np.diag(tmp_diag3 * tmp_diag2))))
 
         self.U = spr.bsr_matrix(np.outer(np.ones(self.n), self.eco.j0 *
-                                             np.power(self.eco.g_eq,
-                                                      (1 - self.eco.b) / self.eco.b),
-                                             )
-                                )/self.eco.labour_eq
+                                         np.power(self.eco.g_eq,
+                                                  (1 - self.eco.b) / self.eco.b),
+                                         )
+                                ) / self.eco.labour_eq
         self.p_over_g = spr.bsr_matrix(np.diag(self.eco.p_eq / self.eco.g_eq))
         self.g_over_p = spr.bsr_matrix(np.diag(self.eco.g_eq / self.eco.p_eq))
 
@@ -118,15 +129,16 @@ class LinearDynamics:
         return spr.diags(self.beta * self.eco.g_eq / (self.z * self.eco.p_eq)).dot(self.M2)
 
     def forecast_block_Y1(self):
-        return - spr.diags(self.betap).dot(np.sum([spr.kron(canonical_Rn(self.n, i), canonical_Mn(self.n, i, i)) / self.z[i]
-                                      for i in range(self.n)], axis=0))
+        return - spr.diags(self.betap).dot(
+            np.sum([spr.kron(canonical_Rn(self.n, i), canonical_Mn(self.n, i, i)) / self.z[i]
+                    for i in range(self.n)], axis=0))
 
     def forecast_block_X2(self):
         return - spr.diags((self.beta + self.betap) * self.eco.cons_eq / (self.z * self.eco.p_eq))
 
     def forecast_block_Y2(self):
         return - spr.diags(self.betap + self.beta).dot(np.sum(
-            [spr.kron(canonical_Rn(self.n, i), spr.diags(1./self.z).dot(spr.eye(self.n) - canonical_Mn(self.n, i, i)))
+            [spr.kron(canonical_Rn(self.n, i), spr.diags(1. / self.z).dot(spr.eye(self.n) - canonical_Mn(self.n, i, i)))
              for i in range(self.n)], axis=0))
 
     def forecast_block_Z2(self):
@@ -162,8 +174,10 @@ class LinearDynamics:
         fst = spr.diags(self.alpha).dot(self.p_over_g) - self.omega * spr.diags(self.eco.p_eq).dot(self.U) \
               / self.eco.b
         snd = spr.diags(self.eco.p_eq / (self.z * self.eco.g_eq)).dot(
-            spr.diags(self.alphap).dot(self.M1.transpose()) / self.eco.b - spr.diags(self.alpha).dot(self.M2.transpose()))
-        thd = - self.eco.labour_eq * spr.diags(self.alphap * self.eco.p_eq * self.eco.cons_eq / (self.z * self.eco.g_eq)).dot(self.U) / \
+            spr.diags(self.alphap).dot(self.M1.transpose()) / self.eco.b - spr.diags(self.alpha).dot(
+                self.M2.transpose()))
+        thd = - self.eco.labour_eq * spr.diags(
+            self.alphap * self.eco.p_eq * self.eco.cons_eq / (self.z * self.eco.g_eq)).dot(self.U) / \
               (self.eco.b * self.eco.b_eq)
 
         return fst + snd + thd
@@ -177,8 +191,8 @@ class LinearDynamics:
 
     def fixed_shortage_block_D(self):
         fst = spr.diags((self.alphap - self.alpha) * self.eco.p_eq /
-                                                     (self.z * self.eco.g_eq)).dot(spr.kron(np.ones(self.n),
-                                                                                            spr.eye(self.n)))
+                        (self.z * self.eco.g_eq)).dot(spr.kron(np.ones(self.n),
+                                                               spr.eye(self.n)))
         snd = - spr.diags(self.alphap / (self.z * self.eco.g_eq)).dot(
             np.sum([spr.kron(canonical_Rn(self.n, i),
                              np.outer(canonical_Rn(self.n, i), self.eco.p_eq))
@@ -257,3 +271,9 @@ class LinearDynamics:
 
     def fixed_dynamical(self):
         return self.forecast_matrix().dot(self.fixed_shortage())
+
+    def get_eigenvalues_fixed_dynamical(self):
+        return np.linalg.eigvals(self.fixed_dynamical().toarray())
+
+    def get_eigenvalues_theoretical_infinite_eps(self):
+        return
